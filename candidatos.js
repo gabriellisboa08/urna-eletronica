1988e62c65a85ecf6b44e67961c68de857820f66
Gabriel Lisboa da Silva <gabriellisboa08@gmail.com>
2021-12-27 21:28:35 -0300

    insert-audio

--- candidatos.js
+++ candidatos.js
@@ -10,18 +10,18 @@ let prefeitos = [];
 let vereadores = [];
 
 prefeitos.push(
-    new Candidatos("74", "Josué da Silva", "PILANTRAS", "./prefeito-1.jpg")
+    new Candidatos("74", "Josué da Silva", "PILANTRAS", "img/prefeito-1.jpg")
 );
 prefeitos.push(
-    new Candidatos("68", "Vouti roubei", "Safados", "./prefeito-2.jpg")
+    new Candidatos("68", "Vouti roubei", "Safados", "img/prefeito-2.jpg")
 );
 
 vereadores.push(
-    new Candidatos("12345", "Zé Mão boba", "corruPTos", "./prefeito-1.jpg")
+    new Candidatos("12345", "Zé Mão boba", "corruPTos", "img/prefeito-1.jpg")
 );
 
 vereadores.push(
-    new Candidatos("54321", "João ladrão ", "pé de feijão", "./prefeito-2.jpg")
+    new Candidatos("54321", "João ladrão ", "pé de feijão", "img/prefeito-2.jpg")
 );
 let candidatos = [];
 candidatos.push(prefeitos, vereadores);
--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 let numero = "";
+const audioBotao = document.querySelector("#audio-botão");
+const audioBotaoConfirma = document.querySelector("#audio-botão-confirma");
+let spanName = document.querySelector("#span-nome");
+let spanPartido = document.querySelector("#span-partido");
+let imgPrincipal = document.querySelector("#img-prefeito");
 
 function clickedNumber(n) {
+     audioBotao.play();
     if (numero.length < 5) {
+        if(numero.length < 1) {
+
+        }
         let selectedElement = document.querySelector(".pisca");
         selectedElement.innerHTML = n;
         numero += n;
@@ -9,16 +18,16 @@ function clickedNumber(n) {
         if (selectedElement.nextElementSibling != null) {
             selectedElement.nextElementSibling.classList.add("pisca");
         }
-        if (numero.length == 5) {
-            let vereadorEscolhido = vereadores.filter(
-                (item) => item.nCandidatura == numero
-            );
-            if (vereadorEscolhido.length == 0) {
-                alert("voto nulo?");
-            } else {
-                console.log(vereadorEscolhido);
-                mostrarCandidato(vereadorEscolhido);
-            }
+    }
+    if (numero.length === 5) {
+        let vereadorEscolhido = vereadores.filter(
+            (item) => item.nCandidatura == numero
+        );
+        if (vereadorEscolhido.length == 0) {
+            alert("voto nulo?");
+        } else {
+            console.log(vereadorEscolhido);
+            mostrarCandidato(vereadorEscolhido);
         }
     }
 }
@@ -27,9 +36,23 @@ function mostrarCandidato(vereadorEscolhido) {
     let spanName = document.querySelector("#span-nome");
     let spanPartido = document.querySelector("#span-partido");
     let imgPrincipal = document.querySelector("#img-prefeito");
-
     console.log(spanName, spanPartido, imgPrincipal);
     spanName.innerText = vereadorEscolhido[0].nome;
     imgPrincipal.innerHTML = `<img id="image-img-principal" src='${vereadorEscolhido[0].foto.url}'></img>`;
     spanPartido.innerText = vereadorEscolhido[0].partido;
 }
+function corrige() {
+    document
+        .querySelectorAll(".num-digitado")
+        .forEach((item) => (item.innerText = ""));
+    imgPrincipal.innerHTML = null
+    spanName.innerText = null
+    spanPartido.innerText = null
+    document.querySelector("#num-digitado-1").classList.add("pisca")
+    numero = ""
+}
+
+function atualizandoVotoNulo() {
+    
+}
+
